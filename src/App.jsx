e269fdbd59564b05c365ad69ec175fa219477d87
jin young <128350568+wlsud801@users.noreply.github.com>
2023-04-17 21:30:31 +0900

    State 통합하기, 완료 or 완료취소 버튼 통합

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,50 @@
 import React from 'react';
 import { useState } from 'react';
 import Working from './component/WorkList';
-import Done from './component/DoneList';
 import './App.css';
-
 
 const App = () => {
   const [work, setWork] = useState([
     { id: 1, title: '리액트 공부하기', contents: '리액트를 열심히 공부해봅시다!', isDone: false },
-    { id: 2, title: '리액트 공부하기', contents: '리액트를 열심히 공부해봅시다!', isDone: false }
+    { id: 2, title: '리액트 공부하기', contents: '리액트를 열심히 공부해봅시다!', isDone: false },
+    { id: 3, title: '자바스크립트 공부하기', contents: '자바스크립트를 열심히 공부해보자', isDone: true }
   ])
 
-  const [done, setDone] = useState([
-    { id: 1, title: '자바스크립트 공부하기', contents: '자바스크립트를 열심히 공부해보자', isDone: true }
-  ])
+  // 입력 받은 값을 저장해두는 State
+  const [listInfo, setListInfo] = useState({ title: '', contents: '' });
 
-  const [title, setTitle] = useState('');
-  const [contents, setContents] = useState('');
-
-  const addTitleHandler = (e) => {
-    setTitle(e.target.value);
-  }
-  const addContentHandler = (e) => {
-    setContents(e.target.value);
+  // 입력받은 값을 listInfo state에 저장 해주기
+  const addInputHandler = (e) => {
+    setListInfo({ ...listInfo, [e.target.name]: e.target.value })
   }
 
-  // 등록하기 버튼
+  // listInfo에 저장된 값을 work State에 업데이트해주기
   const registerBtn = () => {
     const list = {
       id: work.length + 1,
-      title,
-      contents,
-    };
-
-    if(title.length === 0 || contents.length === 0){
-      alert('내용을 입력해주세요')
-    } else {
-      setWork([...work, list]);
-      setTitle('');
-      setContents('');
+      title: listInfo.title,
+      contents: listInfo.contents,
+      isDone: false
     }
 
+    setWork([...work, list])
+    setListInfo({ title: '', contents: '' })
+  }
+
+  // 완료 OR 완료취소 버튼
+  const completeChkBtn = (item) => {
+    if(item.isDone === false){
+      console.log('yes')
+      setWork([...work, item.isDone = true])
+    } else {
+      setWork([...work, item.isDone = false])
+    }
     
   }
 
   // 삭제하기 버튼
-  const removeBtn = (id, complete) => {
-    if (complete !== true) {
-      setWork(work.filter(work => work.id !== id));
-    } else {
-      setDone(done.filter(done => done.id !== id));
-    };
-
-  }
-
-  // 완료 상태 취소하기 버튼
-  const todoBtn = (id, title, contents) => {
-    const list = {
-      id: work.length + 1,
-      title,
-      contents,
-      isDone: false
-    };
-    setDone(done.filter(a => a.id !== id));
-    setWork([...work, list]);
-  }
-
-  // 완료하기 버튼
-  const completeBtn = (id, title, contents) => {
-    const list = {
-      id: done.length + 1,
-      title,
-      contents,
-      isDone: true
-
-    };
+  const removeBtn = (id) => {
     setWork(work.filter(work => work.id !== id));
-    setDone([...done, list]);
   }
 
   return (
@@ -87,28 +55,24 @@ const App = () => {
           <p>React.js</p>
         </header>
         <article>
-          <form name="todoRegister" className="register">
-            <label htmlFor='title' className="red">제목</span><input name="title" onChange={addTitleHandler}  value={title} placeholder="제목을 입력하세요" autoFocus/>
-            <label htmlFor='contnets' className="red">내용</span><input name="contents" onChange={addContentHandler} value={contents} placeholder="내용을 입력하세요"/>
-            <button type="submit" onClick={registerBtn}>추가하기</button>
-          </form>
+          <div className="register">
+            <label htmlFor='title' className="red">제목</label><input name="title" onChange={addInputHandler} value={listInfo.title} placeholder="제목을 입력하세요" autoFocus />
+            <label htmlFor='contnets' className="red">내용</label><input name="contents" onChange={addInputHandler} value={listInfo.contents} placeholder="내용을 입력하세요" />
+            <button type='submit' onClick={registerBtn}>추가하기</button>
+          </div>
           <h2>Working</h2>
           <section className="list">
             {
-              work.filter((item) => {
-                return item.isDone === false;
-              }).map((item) => {
-                return (<Working key={item.id} item={item} completeBtn={completeBtn} removeBtn={removeBtn} />)
+              work.filter((item) => item.isDone === false).map((item) => {
+                return (<Working key={item.id} item={item} completeChkBtn={completeChkBtn} removeBtn={removeBtn} />)
               })
             }
           </section>
           <h2>Done</h2>
           <section className="list">
             {
-              work.filter((item) => {
-                return item.isDone === true;
-              }).map((item) => {
-                return (<Working key={item.id} item={item} todoBtn={todoBtn} removeBtn={removeBtn} />)
+              work.filter((item) => item.isDone === true).map((item) => {
+                return (<Working key={item.id} item={item} completeChkBtn={completeChkBtn} removeBtn={removeBtn} />)
               })
             }
           </section>
